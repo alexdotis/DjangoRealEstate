a8a96f4c7bb2f3a282e11f7eecc02a28ff658c64
alexdotis <43515852+alexdotis@users.noreply.github.com>
2021-09-15 21:10:37 +0300

    Update urls.py

--- RealEstate/RealEstate/urls.py
+++ RealEstate/RealEstate/urls.py
@@ -5,12 +5,13 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.defaults import page_not_found
-from listing.views import  HomepageView
+
+# from listing.views import  HomepageView
 from django.views import generic
 
 
-handler404 = 'listing.views.handler404'
-handler500 = 'listing.views.handler500'
+# handler404 = 'listing.views.handler404'
+# handler500 = 'listing.views.handler500'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -20,12 +21,12 @@ urlpatterns = [
     path('about/',generic.TemplateView.as_view(template_name='pages/about.html'),name='about'),
     path('faq/',generic.TemplateView.as_view(template_name='pages/faq.html'),name='faq'),
     path('contact/',generic.TemplateView.as_view(template_name='pages/contact.html'),name='contact'),
-    path('listing/', include('listing.urls')),
-    path('', HomepageView.as_view(), name='homepage'),
-    path('', include('authentication.urls')),
-    path('profile/', include('userprofile.urls')),
-    path('agency/', include('agency.urls')),
-    path('blogs/', include('blogs.urls')),
+#     path('listing/', include('listing.urls')),
+#     path('', HomepageView.as_view(), name='homepage'),
+#     path('', include('authentication.urls')),
+#     path('profile/', include('userprofile.urls')),
+#     path('agency/', include('agency.urls')),
+#     path('blogs/', include('blogs.urls')),
 
 
 ]
