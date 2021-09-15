e0fb0a0d96d576b7f0143afbd908c7c3a212be9d
alexdotis <43515852+alexdotis@users.noreply.github.com>
2021-09-16 02:06:20 +0300

    Update urls.py

--- RealEstate/listing/urls.py
+++ RealEstate/listing/urls.py
@@ -5,16 +5,13 @@ from .views import (
     ApiListingReviewList,
     ApiCreateListingReview
 )
-# from django.views.generic import TemplateView
+
 
 urlpatterns = [
-   
-    # path('',TemplateView.as_view(template_name='Listing/index.html'),name='listing'),
     path('', ListingListView.as_view(), name='listing'),
     path('<slug>', ListingDetailView.as_view(), name='detail'),
     path('api/property/<slug>/review', ApiListingReviewList.as_view()),
     path('api/property/<slug>/review/create',
          ApiCreateListingReview.as_view()),
-
 
 ]
